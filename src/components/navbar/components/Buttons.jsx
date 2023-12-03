ca670d05815fdd7ddca6ca99fe9c8aeb036aedf6
Oscar <ohohurtado16@gmail.com>
2023-12-03 00:14:06 -0500

    feat :sparkles: implement React-Tanstacktable and testdata

--- src/components/navbar/components/Buttons.jsx
+++ src/components/navbar/components/Buttons.jsx
@@ -19,30 +19,3 @@ const Buttons = ({ icono, texto, onClick, isActive }) => {
 export default Buttons;
 
 
-/* import { useState } from "react";
-
-
-const Buttons = ({ icono, texto }) =>{
-
-    const [color , setColor] = useState(true);
-
-    const handleColor= () =>{
-        setColor(!color);
-    }
-
-  return (
-    <>                 
-        <button className={color ? 'button-navbar' : 'button-navbar-clicked'} onClick={handleColor}>
-                <div className='flex justify-center items-center mx-5 gap-10 '>
-                    {icono}
-                    <span className="dark:hover:text-white">{texto}</span>                        
-                </div>  
-                          
-        </button>       
-    </>
-  );
-
-
-};
-
-export default Buttons; */
